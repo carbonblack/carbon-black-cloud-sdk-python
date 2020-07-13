c7ff183f83d739344e3e3a76ac4542ef09f41f0f
Luke Lyon <llyon@carbonblack.com>
2020-07-13 15:46:07 -0600

    update dir name of src/cbc-sdk for linter

--- bin/linters.sh
+++ bin/linters.sh
@@ -3,5 +3,5 @@
 set -e
 
 echo 'Running flake8....'
-flake8 --docstring-convention google src/cbc_sdk_python
+flake8 --docstring-convention google src/cbc-sdk
 flake8 --docstring-convention google src/tests/
