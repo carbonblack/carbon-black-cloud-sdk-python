a009a160c811a1b8a5608cfee6c9dc5961b7fc42
Luke Lyon <llyon@carbonblack.com>
2020-07-13 16:17:54 -0600

    add dummy files for pytest and coverage

---
+++ src/cbc-sdk/empty_for_coveralls.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# *******************************************************
+# Copyright (c) VMware, Inc. 2020. All Rights Reserved.
+# SPDX-License-Identifier: MIT
+# *******************************************************
+# *
+# * DISCLAIMER. THIS PROGRAM IS PROVIDED TO YOU "AS IS" WITHOUT
+# * WARRANTIES OR CONDITIONS OF ANY KIND, WHETHER ORAL OR WRITTEN,
+# * EXPRESS OR IMPLIED. THE AUTHOR SPECIFICALLY DISCLAIMS ANY IMPLIED
+# * WARRANTIES OR CONDITIONS OF MERCHANTABILITY, SATISFACTORY QUALITY,
+# * NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE.
+
+"""
+This is a dummy Python file to allow coverage to pass for setup.
+
+It will be deleted once real code is present
+"""
+
+if True:
+    pass
---
+++ src/tests/unit/test_empty.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+# *******************************************************
+# Copyright (c) VMware, Inc. 2020. All Rights Reserved.
+# SPDX-License-Identifier: MIT
+# *******************************************************
+# *
+# * DISCLAIMER. THIS PROGRAM IS PROVIDED TO YOU "AS IS" WITHOUT
+# * WARRANTIES OR CONDITIONS OF ANY KIND, WHETHER ORAL OR WRITTEN,
+# * EXPRESS OR IMPLIED. THE AUTHOR SPECIFICALLY DISCLAIMS ANY IMPLIED
+# * WARRANTIES OR CONDITIONS OF MERCHANTABILITY, SATISFACTORY QUALITY,
+# * NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE.
+
+"""Dummy test file for codeship setup. Will be deleted once tests exist."""
+
+
+def test_codeship_pass():
+    """An empty test to allow pytest to pass without returning an error"""
+    return True
