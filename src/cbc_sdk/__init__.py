02693bbc6613ed344577b0a34e44d099fc03e2ca
Luke Lyon <llyon@carbonblack.com>
2020-07-15 12:00:47 -0600

    remove attrdict, replace with attrdict_cbc to fix DeprecationWarning

--- src/cbc_sdk/__init__.py
+++ src/cbc_sdk/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 
-__title__ = 'cbc-sdk'
+__title__ = 'cbc_sdk'
 __author__ = 'Carbon Black Developer Network'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2020-2020 VMware Carbon Black'
