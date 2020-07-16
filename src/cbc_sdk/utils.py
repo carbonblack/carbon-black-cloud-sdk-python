b20d16ddeda0aeb13a620b391ddd28ac61a80a8a
Amy Bowersox <abowersox@vmware.com>
2020-07-16 16:25:21 -0600

    added dynamic loader functions to utils, cleaned up and added unit tests

--- src/cbc_sdk/utils.py
+++ src/cbc_sdk/utils.py
@@ -11,12 +11,26 @@
 # * WARRANTIES OR CONDITIONS OF MERCHANTABILITY, SATISFACTORY QUALITY,
 # * NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE.
 
+"""Utility functions for use within the CBC SDK."""
+
 from __future__ import absolute_import
-import sys
 import dateutil.parser
+from importlib import import_module
+
+
+cb_datetime_format = "%Y-%m-%d %H:%M:%S.%f"
 
 
 def convert_query_params(qd):
+    """
+    Expand a dictionary of query parameters by turning "list" values into multiple pairings of key with value.
+
+    Args:
+        qd (dict): A mapping of parameter names to values.
+
+    Returns:
+        list: A list of query parameters, each one a tuple containing name and value, after the expansion is applied.
+    """
     o = []
     for k, v in iter(qd.items()):
         if type(v) == list:
@@ -29,7 +43,15 @@ def convert_query_params(qd):
 
 
 def convert_from_cb(s):
-    # Use dateutil.parser to parse incoming dates; flexible on what we receive, strict on what we send.
+    """
+    Parse a date and time value into a datetime object.
+
+    Args:
+        s (str): The date and time string to parse. If this is None, we use the UNIX epoch timestamp.
+
+    Returns:
+        datetime: The parsed date and time.
+    """
     if s is None:
         return dateutil.parser.parse("1970-01-01T00:00:00Z")
     else:
@@ -37,4 +59,50 @@ def convert_from_cb(s):
 
 
 def convert_to_cb(dt):
+    """
+    Convert a date and time to a string in the Carbon Black format.
+
+    Args:
+        dt (datetime): The date and time to be converted.
+
+    Returns:
+        str: The date and time as a string.
+    """
     return dt.strftime(cb_datetime_format)
+
+
+def dynamic_load(full_class_name):
+    """
+    Loads a Python class object given its fully-qualified class name.
+
+    Args:
+        full_class_name (str): The class name of the object to be loaded.
+
+    Returns:
+        The class object.
+
+    Raises:
+        ImportError: If the class could not be loaded.
+
+    """
+    try:
+        module_path, class_name = full_class_name.rsplit('.', 1)
+        module = import_module(module_path)
+        return getattr(module, class_name)
+    except (ImportError, AttributeError) as e:
+        raise ImportError(full_class_name) from e
+
+
+def dynamic_create(full_class_name):
+    """
+    Creates a Python object given its fully-qualified class name.
+
+    Args:
+        full_class_name (str): The class name of the object to be created.
+
+    Returns:
+        A new instance of that object.
+
+    """
+    class_obj = dynamic_load(full_class_name)
+    return class_obj()
