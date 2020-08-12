21a41485d071d044a6002ef381d7dde29b33f4c3
Amy Bowersox <abowersox@vmware.com>
2020-08-12 10:52:08 -0600

    reserve the position for the registry setup script

---
+++ bin/set-registry.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# *******************************************************
+# Copyright (c) VMware, Inc. 2020. All Rights Reserved.
+# SPDX-License-Identifier: MIT
+# *******************************************************
+# *
+# * DISCLAIMER. THIS PROGRAM IS PROVIDED TO YOU "AS IS" WITHOUT
+# * WARRANTIES OR CONDITIONS OF ANY KIND, WHETHER ORAL OR WRITTEN,
+# * EXPRESS OR IMPLIED. THE AUTHOR SPECIFICALLY DISCLAIMS ANY IMPLIED
+# * WARRANTIES OR CONDITIONS OF MERCHANTABILITY, SATISFACTORY QUALITY,
+# * NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE.
+
+"""Registry setup helper script"""
+
+import sys
+
+
+def main(args):
+    pass
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
