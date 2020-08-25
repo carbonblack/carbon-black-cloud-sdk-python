b0e1efc38cc5737b02e3667325498b0f099206d6
Luke Lyon <llyon@carbonblack.com>
2020-08-25 13:41:39 -0600

    overwrite credential and auth files with develop

--- src/cbc_sdk/credential_providers/__init__.py
+++ src/cbc_sdk/credential_providers/__init__.py
@@ -1,3 +1,4 @@
 from .default import default_credential_provider
 from .file_credential_provider import FileCredentialProvider
 from .environ_credential_provider import EnvironCredentialProvider
+from .registry_credential_provider import RegistryCredentialProvider
