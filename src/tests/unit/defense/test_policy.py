c130264d803507847ee9ed459e05c1159f87a604
Luke Lyon <llyon@carbonblack.com>
2020-08-26 13:15:36 -0600

    add test files and fixtures

---
+++ src/tests/unit/defense/test_policy.py
@@ -0,0 +1,87 @@
+"""Testing Devices object of cbc_sdk.defense"""
+
+import pytest
+import logging
+from cbc_sdk.defense import Policy
+from cbc_sdk.defense import Query
+from cbc_sdk.rest_api import CBCloudAPI
+from tests.unit.fixtures.CBCSDKMock import CBCSDKMock
+from tests.unit.fixtures.mock_policy import (POLICY_GET_RESP,
+                                             POLICY_POST_RULE_RESP,
+                                             POLICY_GET_WITH_NEW_RULE_RESP,
+                                             POLICY_MODIFY_RULE_RESP,
+                                             POLICY_GET_WITH_MODIFIED_RULE_RESP,
+                                             POLICY_DELETE_RULE_RESP,
+                                             POLICY_GET_WITH_DELETED_RULE_RESP)
+
+log = logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s', level=logging.DEBUG, filename='log.txt')
+
+
+@pytest.fixture(scope="function")
+def cb():
+    """Create CBCloudAPI singleton"""
+    return CBCloudAPI(url="https://example.com",
+                      org_key="test",
+                      token="abcd/1234",
+                      ssl_verify=False)
+
+
+@pytest.fixture(scope="function")
+def cbcsdk_mock(monkeypatch, cb):
+    """Mocks CBC SDK for unit tests"""
+    return CBCSDKMock(monkeypatch, cb)
+
+
+# ==================================== UNIT TESTS BELOW ====================================
+
+def test_policy_select_modify(cbcsdk_mock):
+    """Testing Policy Querying with .select(Policy, `policy_id`) and add/del rules"""
+    cbcsdk_mock.mock_request("GET", "/integrationServices/v3/policy/30241", POLICY_GET_RESP)
+    api = cbcsdk_mock.api
+    policy = api.select(Policy, 30241)
+    assert policy.id == 30241
+    policy.refresh()
+
+    new_rule = {"action": "DENY", "application": {"type": "NAME_PATH", "value": "my_path_test"}, "operation": "RUN", "required": True}
+    cbcsdk_mock.mock_request("GET", "/integrationServices/v3/policy/30241", POLICY_GET_RESP)
+    rules = policy.rules.values()
+    rule_ids = [rule.pop('id') for rule in rules]
+    rules_without_ids = rules
+    assert new_rule not in rules_without_ids
+
+    cbcsdk_mock.mock_request("POST", "/integrationServices/v3/policy/30241/rule", POLICY_POST_RULE_RESP)
+    cbcsdk_mock.mock_request("GET", "/integrationServices/v3/policy/30241", POLICY_GET_WITH_NEW_RULE_RESP)
+    policy.add_rule(new_rule)
+    rules = policy.rules.values()
+    rule_ids = [rule.pop('id') for rule in rules]
+    new_rule_id = rule_ids[-1]
+    rules_without_ids = rules
+    assert new_rule in rules_without_ids
+
+    modified_rule = {"action": "IGNORE", "application": {"type": "NAME_PATH", "value": "new_test_path"}, "operation": "RUN", "required": True, "id": new_rule_id}
+    cbcsdk_mock.mock_request("PUT", "/integrationServices/v3/policy/30241/rule/22", POLICY_MODIFY_RULE_RESP)
+    cbcsdk_mock.mock_request("GET", "/integrationServices/v3/policy/30241", POLICY_GET_WITH_MODIFIED_RULE_RESP)
+    policy.replace_rule(new_rule_id, modified_rule)
+    rules = policy.rules.values()
+    modified_rule.pop("id")
+    rule_ids = [rule.pop('id') for rule in rules]
+    rules_without_ids = rules
+    assert modified_rule in rules_without_ids
+
+    cbcsdk_mock.mock_request("DELETE", "/integrationServices/v3/policy/30241/rule/22", POLICY_DELETE_RULE_RESP)
+    cbcsdk_mock.mock_request("GET", "/integrationServices/v3/policy/30241", POLICY_GET_WITH_DELETED_RULE_RESP)
+    policy.delete_rule(rule_ids[-1])
+    rules = policy.rules.values()
+    rule_ids = [rule.pop('id') for rule in rules]
+    rules_without_ids = rules
+    assert modified_rule not in rules_without_ids
+
+
+def test_policy_select(cbcsdk_mock):
+    """Testing Policy Querying with .select(Policy)"""
+    cbcsdk_mock.mock_request("GET", "/integrationServices/v3/policy/30241", POLICY_GET_RESP)
+    api = cbcsdk_mock.api
+    policy = api.select(Policy, 30241)
+    assert isinstance(policy, Policy)
+    assert policy.id == 30241
+    assert policy.name == "Lyon_test"
---
+++ src/tests/unit/fixtures/CBCSDKMock.py
@@ -0,0 +1,174 @@
+# -*- coding: utf-8 -*-
+
+# *******************************************************
+# Copyright (c) VMware, Inc. 2020. All Rights Reserved.
+# SPDX-License-Identifier: MIT
+# *******************************************************
+# *
+# * DISCLAIMER. THIS PROGRAM IS PROVIDED TO YOU "AS IS" WITHOUT
+# * WARRANTIES OR CONDITIONS OF ANY KIND, WHETHER ORAL OR WRITTEN,
+# * EXPRESS OR IMPLIED. THE AUTHOR SPECIFICALLY DISCLAIMS ANY IMPLIED
+# * WARRANTIES OR CONDITIONS OF MERCHANTABILITY, SATISFACTORY QUALITY,
+# * NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE.
+
+"""CBCSDK Mock Framework"""
+
+import pytest
+import re
+import copy
+
+
+class CBCSDKMock:
+    """Mock framework for unit tests that need to fetch Carbon Black Cloud data"""
+    def __init__(self, monkeypatch, api):
+        """Initializes monkey patch for HTTP VERB requests"""
+        self.mocks = {}
+        self.monkeypatch = monkeypatch
+        self.api = api
+        self._last_request_data = None
+        self._all_request_data = list()
+        monkeypatch.setattr(api, "get_object", self._self_get_object())
+        monkeypatch.setattr(api, "get_raw_data", self._self_get_raw_data())
+        monkeypatch.setattr(api, "post_object", self._self_post_object())
+        monkeypatch.setattr(api, "put_object", self._self_put_object())
+        monkeypatch.setattr(api, "delete_object", self._self_delete_object())
+        monkeypatch.setattr(api, "api_json_request", self._self_patch_object())
+
+    class StubResponse(object):
+        """Stubbed response to object to support json function similar to requests package"""
+        def __init__(self, contents, scode=200, text=""):
+            """Init default properties"""
+            self._contents = contents
+            self.status_code = scode
+            self.text = text
+
+        def json(self):
+            """Mimics request package"""
+            return self._contents
+
+    def get_mock_key(self, verb, url):
+        """Algorithm for getting/setting mocked VERB + URL"""
+        return "{}:{}".format(verb, url)
+
+    def match_key(self, request):
+        """Matches mocked requests against incoming request"""
+        for key in self.mocks.keys():
+            exp = key.replace("/", ".")
+            matched = re.match(exp, request)
+            if matched:
+                return key
+        return None
+
+    def _capture_data(self, data):
+        self._all_request_data.append(data)
+        self._last_request_data = data
+
+    def mock_request(self, verb, url, body):
+        """
+        Mocks the VERB + URL by defining the response for that particular request
+
+        Args:
+            verb (str): HTTP verb supported [ GET, RAW_GET, POST, PUT, DELETE ]
+            url (str): The full path of to be mocked with support for regex
+            body (?): Any value or object to be returned as mocked response
+
+        Additional Details:
+            When PUT body is None then respond with request body
+
+        """
+        if verb == "GET" or verb == "RAW_GET" or \
+           callable(body) or \
+           body is Exception or body in Exception.__subclasses__():
+            self.mocks["{}:{}".format(verb, url)] = body
+        else:
+            self.mocks["{}:{}".format(verb, url)] = self.StubResponse(body)
+
+    """
+        Factories for mocked API requests
+    """
+    def _self_get_object(self):
+        def _get_object(url, query_parameters=None, default=None):
+            self._capture_data(query_parameters)
+            matched = self.match_key(self.get_mock_key("GET", url))
+            if matched:
+                if callable(self.mocks[matched]):
+                    return self.mocks[matched](url, query_parameters, default)
+                elif self.mocks[matched] is Exception or self.mocks[matched] in Exception.__subclasses__():
+                    raise self.mocks[matched]
+                else:
+                    return self.mocks[matched]
+            pytest.fail("GET called for %s when it shouldn't be" % url)
+        return _get_object
+
+    def _self_post_object(self):
+        def _post_object(url, body, **kwargs):
+            self._capture_data(body)
+            matched = self.match_key(self.get_mock_key("POST", url))
+            if matched:
+                if callable(self.mocks[matched]):
+                    return self.StubResponse(self.mocks[matched](url, body, **kwargs))
+                elif self.mocks[matched] is Exception or self.mocks[matched] in Exception.__subclasses__():
+                    raise self.mocks[matched]
+                else:
+                    return self.mocks[matched]
+            pytest.fail("POST called for %s when it shouldn't be" % url)
+        return _post_object
+
+    def _self_get_raw_data(self):
+        def _get_raw_data(url, query_params, **kwargs):
+            self._capture_data(query_params)
+            matched = self.match_key(self.get_mock_key("RAW_GET", url))
+            if matched:
+                if callable(self.mocks[matched]):
+                    return self.mocks[matched](url, query_params, **kwargs)
+                elif self.mocks[matched] is Exception or self.mocks[matched] in Exception.__subclasses__():
+                    raise self.mocks[matched]
+                else:
+                    return self.mocks[matched]
+            pytest.fail("Raw GET called for %s when it shouldn't be" % url)
+        return _get_raw_data
+
+    def _self_put_object(self):
+        def _put_object(url, body, **kwargs):
+            self._capture_data(body)
+            matched = self.match_key(self.get_mock_key("PUT", url))
+            if matched:
+                response = self.mocks[matched]
+                if response._contents is None:
+                    response = copy.deepcopy(self.mocks[matched])
+                    response._contents = body
+                elif callable(self.mocks[matched]):
+                    return self.StubResponse(self.mocks[matched](url, body, **kwargs))
+                elif self.mocks[matched] is Exception or self.mocks[matched] in Exception.__subclasses__():
+                    raise self.mocks[matched]
+                return response
+            pytest.fail("PUT called for %s when it shouldn't be" % url)
+        return _put_object
+
+    def _self_delete_object(self):
+        def _delete_object(url, body=None):
+            self._capture_data(body)
+            matched = self.match_key(self.get_mock_key("DELETE", url))
+            if matched:
+                if callable(self.mocks[matched]):
+                    return self.StubResponse(self.mocks[matched](url, body))
+                elif self.mocks[matched] is Exception or self.mocks[matched] in Exception.__subclasses__():
+                    raise self.mocks[matched]
+                else:
+                    return self.mocks[matched]
+            pytest.fail("DELETE called for %s when it shouldn't be" % url)
+        return _delete_object
+
+    def _self_patch_object(self):
+        def _patch_object(url, body, **kwargs):
+            self._capture_data(body)
+            matched = self.match_key(self.get_mock_key("PATCH", url))
+            if matched:
+                if callable(self.mocks[matched]):
+                    return self.StubResponse(self.mocks[matched](url, body, **kwargs))
+                elif self.mocks[matched] is Exception or self.mocks[matched] in Exception.__subclasses__():
+                    raise self.mocks[matched]
+                else:
+                    return self.mocks[matched]
+            pytest.fail("PATCH called for %s when it shouldn't be" % url)
+        return _patch_object
