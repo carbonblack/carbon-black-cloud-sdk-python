6359238c5641dbd8c455272523084cbccf66f2ec
Amy Bowersox <abowersox@vmware.com>
2020-09-17 09:22:11 -0600

    added a bunch of tests - live_response_api now at 61%, overall to 79%

---
+++ src/tests/unit/fixtures/live_response/mock_session.py
@@ -0,0 +1,127 @@
+SESSION_INIT_RESP = {
+    "hostname": None,
+    "address": None,
+    "os_version": None,
+    "current_working_directory": None,
+    "supported_commands": None,
+    "drives": None,
+    "id": "1:2468",
+    "sensor_id": 2468,
+    "check_in_timeout": 900,
+    "session_timeout": 900,
+    "sensor_check_in_time": None,
+    "status": "PENDING",
+    "current_command_index": 0,
+    "create_time": 1502126352449
+}
+
+SESSION_POLL_RESP = {
+    "hostname": None,
+    "address": None,
+    "os_version": None,
+    "current_working_directory": "C:\\Windows\\system32",
+    "supported_commands": [
+        "put file",
+        "get file",
+        "memdump",
+        "create directory",
+        "delete file",
+        "directory list",
+        "reg enum key",
+        "reg query value",
+        "reg create key",
+        "reg delete key",
+        "reg delete value",
+        "reg set value",
+        "process list",
+        "kill",
+        "create process"
+    ],
+    "drives": [
+        "A:\\",
+        "C:\\",
+        "D:\\"
+    ],
+    "id": "1:2468",
+    "sensor_id": 2468,
+    "check_in_timeout": 900,
+    "session_timeout": 900,
+    "sensor_check_in_time": 1502126744685,
+    "status": "ACTIVE",
+    "current_command_index": 0,
+    "create_time": 1502126655758
+}
+
+SESSION_POLL_RESP_ERROR = {
+    "hostname": None,
+    "address": None,
+    "os_version": None,
+    "current_working_directory": "C:\\Windows\\system32",
+    "supported_commands": [
+        "put file",
+        "get file",
+        "memdump",
+        "create directory",
+        "delete file",
+        "directory list",
+        "reg enum key",
+        "reg query value",
+        "reg create key",
+        "reg delete key",
+        "reg delete value",
+        "reg set value",
+        "process list",
+        "kill",
+        "create process"
+    ],
+    "drives": [
+        "A:\\",
+        "C:\\",
+        "D:\\"
+    ],
+    "id": "1:2468",
+    "sensor_id": 2468,
+    "check_in_timeout": 900,
+    "session_timeout": 900,
+    "sensor_check_in_time": 1502126744685,
+    "status": "error",
+    "current_command_index": 0,
+    "create_time": 1502126655758
+}
+
+SESSION_CLOSE_RESP = {
+    "hostname": None,
+    "address": None,
+    "os_version": None,
+    "current_working_directory": "C:\\Windows\\system32",
+    "supported_commands": [
+        "put file",
+        "get file",
+        "memdump",
+        "create directory",
+        "delete file",
+        "directory list",
+        "reg enum key",
+        "reg query value",
+        "reg create key",
+        "reg delete key",
+        "reg delete value",
+        "reg set value",
+        "process list",
+        "kill",
+        "create process"
+    ],
+    "drives": [
+        "A:\\",
+        "C:\\",
+        "D:\\"
+    ],
+    "id": "1:2468",
+    "sensor_id": 2468,
+    "check_in_timeout": 900,
+    "session_timeout": 900,
+    "sensor_check_in_time": 1502126744685,
+    "status": "CLOSE",
+    "current_command_index": 0,
+    "create_time": 1502126655758
+}
