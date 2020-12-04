2b1da2ee16f56ed6fbbce213286276d7dd05e31e
Kylie Ebringer <kebringer@carbonblack.com>
2020-12-03 21:22:32 -0700

    Added tests for process facet, enriched events, enriched events facet

--- src/tests/uat/process_search_calls.py
+++ src/tests/uat/process_search_calls.py
@@ -11,26 +11,66 @@
 # * NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE.
 
 """
-Command-line example that retrieves all processes within the last six hours from all active devices.
-Uses asynchronous querying to generate the queries for each device's processes so that they run in parallel.
-"""
+The following API calls are tested in this script.
+Validation is manual; either from information in the console or running an equivalent API call from postman
 
+To execute, a profile must be provided using the standard CBC Credentials.
+
+Processes:
+* Process Search Validation (v1)
+* Get a List of All Available Process Result Sets (v1)
+* Start a Process Search (v2)
+* Get the Status of a Process Search (v1)
+* Retrieve Results for a Process Search (v2)
+* Start a Facet Search on Processes (v2)
+* Retrieve Results for a Process Facets Search (v2)
+
+Process Events:
+* Get Validation for Event Search (v1)
+* Get Events Associated with a Given Process (v2)
+
+Enriched Events
+* Start an Enriched Events Search (v2)
+* Get the Enriched Events Search Status (v1)
+* Retrieve Results for an Enriched Events Search (v2)
+* Retrieve Results for an Enriched Events Facet Search (v2)
+
+The following calls will be added soon.  Work on the SDK is in progress.
+* Get Events Facet Associated with a Process (v2)
+* Start Aggregation Search on Enriched Events (v1)
+* Retrieve Results for an Enriched Event Aggregation Search (v1)
+* Request Details for Enriched Events (v2)
+* Get the Enriched Events Detail Search Status (v2)
+* Retrieve Results for an Enriched Event Detail Search (v2)
+
+The following calls will be added when the API on CBC is complete
+* Request Details of Processes (v2)
+* Get the Status of a Process Detail Search (v2)
+* Retrieve Results for a Process Detail Search (v2)
+* Start a Process Summary Search (v2)
+* Get the Status of Process Summary Search (v2)
+* Retrieve Results for a Process Summary or Tree Search (v2)
+
+The following calls are not planned to be included in the SDK.
+If there is customer demand, this can be reviewed.
+* Process Search Suggestions (v1)
+* Get Time Limits for Available Data (v1)
+* Get Suggestions for Event Searching (v1)
+* Calls using Enterprise EDR Watchlist Features
+* Evaluate Processes for a Watchlist (v1)
+* Get Report Hits (v1)
+
+"""
+import pprint
 import sys
 from cbc_sdk.helpers import build_cli_parser, get_cb_cloud_object, CBCloudAPI
-from cbc_sdk.enterprise_edr import Process, Event, ProcessFacet
+from cbc_sdk.enterprise_edr import Process, Event, ProcessFacet  # , EventFacet
+from cbc_sdk.endpoint_standard import EnrichedEvent, EnrichedEventFacet
 from cbc_sdk.errors import ApiError
 
 
-def print_test_coverage():
-    print("Following API calls are not tested in UAT set:")
-    print("  * Process Search Suggestions (v1) - Primarily for UI, not included in SDK")
-    print("\n")
-    print("Following API calls are covered in UAT:")
-    print("  * Process Search Validation (v1) ")
-
-
-def process_invalid_search(cb, print_detail=False):
-    '''
+def run_process_invalid_search(cb, print_detail=False):
+    """
     Tests that Process Search Validation is being called by putting an invalid field name in the where clause
 
     Args:
@@ -38,20 +78,41 @@ def process_invalid_search(cb, print_detail=False):
         print_detail (bool): whether to print full info to the console, useful for debugging
 
     Returns:
-    '''
+    """
     print(f"Executing process_invalid_search")
     process_query = cb.select(Process).where("enrichedBADFIELD:true")
     try:
-        matching_processes = [process for process in
-                              process_query._perform_query(process_query._perform_query(numrows=10))]
+        matching_processes = [process for process in process_query]
     except ApiError as e:
         print("Expected result: APIError generated")
-        if(print_detail):
+        if print_detail:
             print(e)
     print(f"End: process_invalid_search")
 
-def get_list_of_process_results(cb, print_details=False):
-    '''
+
+def run_process_event_invalid_search(cb, print_detail=False):
+    """
+    Tests that Event Search Validation is being called by putting an invalid field name in the where clause
+
+    Args:
+        cb (CBCloudAPI): API object
+        print_detail (bool): whether to print full info to the console, useful for debugging
+
+    Returns:
+    """
+    print(f"Executing run_process_event_invalid_search")
+    event_query = cb.select(Event).where("enrichedBADFIELD:true")
+    try:
+        matching_events = [e for e in event_query]
+    except ApiError as e:
+        print("Expected result: APIError generated")
+        if print_detail:
+            print(e)
+    print(f"End: run_process_event_invalid_search")
+
+
+def get_list_of_available_process_results(cb, print_details=False):
+    """
     Executes Get a List of All Available Process Result Sets
     Compare to results of this route, no body, no parameters.
     {{base_url}}/api/investigate/v1/orgs/{{org_key}}/processes/search_jobs
@@ -62,15 +123,14 @@ def get_list_of_process_results(cb, print_details=False):
 
     Returns:
 
-    '''
+    """
     process_queries = cb.fetch_process_queries()
     print(f"there were {len(process_queries)} process queries found")
     print("called fetch_process_queries")
 
 
-
-def process_basic_window_enriched(cb, print_detail=False,window="-3d"):
-    '''
+def get_process_basic_window_enriched(cb, print_detail=False, window="-3d"):
+    """
     Args:
         cb (CBCloudAPI): API object
         print_detail (bool): whether to print full info to the console, useful for debugging
@@ -78,23 +138,23 @@ def process_basic_window_enriched(cb, print_detail=False,window="-3d"):
 
     Returns:
         process_guid of the first process in the returned list
-    '''
-    print(f"Executing process_basic_window_enriched")
+    """
+    print(f"Executing get_process_basic_window_enriched")
     process_query = cb.select(Process).where("enriched:true")
     process_query.set_time_range(window=window)
-    matching_processes = [process for process in process_query._perform_query(numrows=10000)]
+    matching_processes = [process for process in process_query]
     print(f"There are {len(matching_processes)} found in {window} of processes")
     if print_detail:
         for process in matching_processes:
             print("{0:16} {1:5} {2:20}".format(process.device_name, process.process_pids[0], process.process_guid))
 
-    print(f"End: process_basic_window_enriched")
+    print(f"End: get_process_basic_window_enriched")
     # return the first process id for use in future tests
-    return matching_processes[10].process_guid
+    return matching_processes[6].process_guid
 
 
-def process_events_for_single_process(cb, print_detail, guid):
-    '''
+def get_process_events_for_single_process(cb, print_detail, guid):
+    """
 
     Args:
         cb (CBCloudAPI): API object
@@ -103,12 +163,15 @@ def process_events_for_single_process(cb, print_detail, guid):
 
     Returns:
 
-    '''
+    """
+    print("Start: get_process_events_for_single_process")
     events_query = cb.select(Event).where(process_guid=guid)
     events_query.sort_by("backend_timestamp", direction="ASC")
-    events = [ev for ev in events_query._perform_query(numrows=10000)]
-    print(f"\n events_query has {len(events_query)} in events_query._total_results")
-    print(f"events_query._total_segments = {events_query._total_segments} and vents_query._processed_segments = {events_query._processed_segments}")
+    events = [ev for ev in events_query]
+    print(f"events_query has {len(events_query)} in len(events_query)")
+    print(f"events_query has {events_query._total_results} in events_query._total_results")
+    print(
+        f"events_query._total_segments = {events_query._total_segments} and events_query._processed_segments = {events_query._processed_segments}")
     print(
         f"There are {len(events)} to print. First timestamp = {events[0].event_timestamp}.  Last timestamp = {events[len(events) - 1].event_timestamp}")
     if print_detail:
@@ -119,9 +182,10 @@ def process_events_for_single_process(cb, print_detail, guid):
             print(
                 f"{event.event_guid},{event.event_hash},{event.process_guid},{event.backend_timestamp},{event.event_timestamp}")
 
-    print(f"Function End")
+    print("End: get_process_events_for_single_process")
 
-def process_facet(cb, print_details=False,window="-3d"):
+
+def get_process_facet(cb, print_detail=False, window="-3d"):
     """
     Exercises Process Facet search; start and get results.
 
@@ -132,33 +196,152 @@ def process_facet(cb, print_details=False,window="-3d"):
 
     Returns:
     """
-    print(f"process_facet Start")
-    facet_query = cb.select(ProcessFacet).where("process_name:svchost.exe")
+    print(f"Start: get_process_facet")
+    facet_query = cb.select(ProcessFacet).where("process_name:chrome.exe")
     facet_query.add_range({"bucket_size": "+1DAY",
-               "start": "2020-11-01T00:00:00Z",
-               "end": "2020-11-30T00:00:00Z",
-               "field": "backend_timestamp"})
-    facet_query.add_facet_field(["alert_category","device_external_ip","backend_timestamp"]).timeout(60000)
+                           "start": "2020-11-01T00:00:00Z",
+                           "end": "2020-11-30T00:00:00Z",
+                           "field": "backend_timestamp"})
+    facet_query.add_facet_field(["alert_category", "device_external_ip", "backend_timestamp"]).timeout(60000)
     facet_query.set_time_range(window=window)
-    # TODO - figure out how to call facets and get results
-    #  facets = facet_query._perform_query()
+    facet_query.set_rows(5)
+    future = facet_query.execute_async()
+    while not future.done():
+        if print_detail:
+            print(f"{future.done()} - looping")
 
-    print(f"INCOMPLETE process_facet End")
+    results = future.result()[0]
+    print(f"terms facets = ")
+    pprint.pprint(results.terms_.facets)
+    print(f"terms fields = {results.terms_.fields}")
+    print(f"ranges facets = {results.ranges_.facets}")
+    print(f"ranges fields = {results.ranges_.fields}")
+
+    print(f"End: get_process_facet")
+
+
+def get_event_facet(cb, print_detail=False, window="-3d"):
+    """
+    Exercises Process Facet search; start and get results.
+
+    Args:
+        cb (CBCloudAPI): API object
+        print_detail (bool): whether to print full info to the console, useful for debugging
+        window (str): period to search
+
+    Returns:
+    """
+    print(f"Start: get_event_facet")
+    print("Not implemented in SDK")
+    # facet_query = cb.select(EventFacet).where("process_name:chrome.exe")
+    # facet_query.add_range({"bucket_size": "+1DAY",
+    #                        "start": "2020-11-01T00:00:00Z",
+    #                        "end": "2020-11-30T00:00:00Z",
+    #                        "field": "backend_timestamp"})
+    # facet_query.add_facet_field(["alert_category", "device_external_ip", "backend_timestamp"]).timeout(60000)
+    # facet_query.set_time_range(window=window)
+    # facet_query.set_rows(5)
+    # future = facet_query.execute_async()
+    # while not future.done():
+    #     if print_detail:
+    #         print(f"{future.done()} - looping")
+    #
+    # results = future.result()[0]
+    # print(f"terms facets = ")
+    # pprint.pprint(results.terms_.facets)
+    # print(f"terms fields = {results.terms_.fields}")
+    # print(f"ranges facets = {results.ranges_.facets}")
+    # print(f"ranges fields = {results.ranges_.fields}")
+
+    print(f"End: get_event_facet")
+
+
+def get_enriched_events_for_single_process(cb, print_detail, guid):
+    """
+
+    Args:
+        cb (CBCloudAPI): API object
+        print_detail (bool): whether to print full info to the console, useful for debugging
+        guid: All enriched events retrieved for this process guid
+
+    Returns:
+
+    """
+    print("Start: get_enriched_events_for_single_process")
+    enriched_events_query = cb.select(EnrichedEvent).where(process_guid=guid)
+    events = [ev for ev in enriched_events_query]
+    print(f"enriched events_query has {len(enriched_events_query)} in len(enriched_events_query")
+    print(f"enriched events_query has {enriched_events_query._total_results} in enriched_events_query._total_results")
+    print("End: get_enriched_events_for_single_process")
+
+
+def get_enriched_event_facet(cb, print_detail=False, window="-3d"):
+    """
+    Exercises Process Facet search; start and get results.
+
+    Args:
+        cb (CBCloudAPI): API object
+        print_detail (bool): whether to print full info to the console, useful for debugging
+        window (str): period to search
+
+    Returns:
+    """
+    print(f"Start: get_enriched_event_facet")
+    facet_query = cb.select(EnrichedEventFacet).where("process_name:chrome.exe")
+    facet_query.add_range({"bucket_size": "+1DAY",
+                           "start": "2020-11-01T00:00:00Z",
+                           "end": "2020-11-30T00:00:00Z",
+                           "field": "backend_timestamp"})
+    facet_query.add_facet_field(["alert_category", "device_external_ip", "backend_timestamp"]).timeout(60000)
+    facet_query.set_time_range(window=window)
+    facet_query.set_rows(5)
+    future = facet_query.execute_async()
+    while not future.done():
+        if print_detail:
+            print(f"{future.done()} - looping")
+
+    results = future.result()[0]
+    print(f"terms facets = ")
+    pprint.pprint(results.terms_.facets)
+    print(f"terms fields = {results.terms_.fields}")
+    print(f"ranges facets = {results.ranges_.facets}")
+    print(f"ranges fields = {results.ranges_.fields}")
+
+    print(f"End: get_enriched_event_facet")
+
 
 def main():
     parser = build_cli_parser()  # args on command line will be applied to all tests called
     args = parser.parse_args()
     print_detail = args.verbose
+    do_process = True
+    do_enriched_events = True
     window = "-3d"
     cb = get_cb_cloud_object(args)
     if print_detail:
         print(f"profile being used is {args.__dict__}")
-    process_invalid_search(cb,print_detail)
-    get_list_of_process_results(cb,print_detail)
-    # TO DO test not complete.  process_facet(cb,print_detail,window)
-    process_guid = process_basic_window_enriched(cb, print_detail,window)
-    print(f"process guid being used is {process_guid}")
-    process_events_for_single_process(cb, print_detail, process_guid)
+    process_guid = ""
+    if do_process:
+        process_guid = get_process_basic_window_enriched(cb, print_detail, window)
+        print(f"process guid being used is {process_guid}")
+        print("----------------------------------------------------------")
+        get_process_events_for_single_process(cb, print_detail, process_guid)
+        print("----------------------------------------------------------")
+        run_process_invalid_search(cb, print_detail)
+        print("----------------------------------------------------------")
+        run_process_event_invalid_search(cb, print_detail)
+        print("----------------------------------------------------------")
+        get_process_facet(cb, print_detail, window)
+        print("----------------------------------------------------------")
+        get_list_of_available_process_results(cb, print_detail)
+        print("----------------------------------------------------------")
+    if do_enriched_events:
+        if process_guid == "":
+            process_guid = "WNEXFKQ7-00050603-00000270-00000000-1d6c86e280fbff8"
+        get_enriched_events_for_single_process(cb, print_detail, process_guid)
+        print("----------------------------------------------------------")
+        get_enriched_event_facet(cb, print_detail, window)
+        print("----------------------------------------------------------")
 
 
 if __name__ == "__main__":
