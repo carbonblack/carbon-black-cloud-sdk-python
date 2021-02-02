fda8615d344260cd175a332bb44ff22e0decb90e
Alex Van Brunt <avanbrunt@carbonblack.com>
2021-02-02 12:42:03 -0700

    Release v1.1.1

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2020, Developer Relations'
 author = 'Developer Relations'
 
 # The full version, including alpha/beta/rc tags
-release = '1.1.0'
+release = '1.1.1'
 
 
 # -- General configuration ---------------------------------------------------
