3ddb47155653b93dbb7170d56e1334b00329fa83
rmcqueen <rmcqueen@vmware.com>
2021-10-07 22:59:46 -0400

    Normalized substitution of orgKey value for `urlobject` strings that have a single format field

--- src/cbc_sdk/base.py
+++ src/cbc_sdk/base.py
@@ -349,6 +349,7 @@ class BinaryFieldDescriptor(FieldDescriptor):
 
 class NewBaseModel(object, metaclass=CbMetaModel):
     """Base class of all model objects within the Carbon Black Cloud SDK."""
+    urlobject = ""
     primary_key = "id"
 
     # Constants for tuning string representations
@@ -369,6 +370,12 @@ class NewBaseModel(object, metaclass=CbMetaModel):
         self._cb = cb
         self._last_refresh_time = 0
         self._max_name_len = 0
+
+        urlobject = self.urlobject
+        try:
+            self._urlobject = urlobject.format(self._cb.credentials.org_key)
+        except (IndexError, KeyError):
+            self._urlobject = urlobject
 
         if initial_data is not None:
             self._info = initial_data
@@ -516,7 +523,7 @@ class NewBaseModel(object, metaclass=CbMetaModel):
         return False
 
     def _build_api_request_uri(self, http_method="GET"):
-        baseuri = self.__class__.__dict__.get('urlobject', None)
+        baseuri = self._urlobject
         if self._model_unique_id is not None:
             return baseuri + "/%s" % self._model_unique_id
         else:
@@ -813,7 +820,7 @@ class MutableBaseModel(NewBaseModel):
                 pass
             log.debug("Creating a new {0:s} object".format(self.__class__.__name__))
             http_method = self.__class__._new_object_http_method
-            ret = self._cb.api_json_request(http_method, self.urlobject,
+            ret = self._cb.api_json_request(http_method, self._urlobject,
                                             data=new_object_info)
         else:
             log.debug("Updating {0:s} with unique ID {1:s}".format(self.__class__.__name__, str(self._model_unique_id)))
@@ -1064,11 +1071,13 @@ class SimpleQuery(BaseQuery, IterableQueryMixin):
         super(SimpleQuery, self).__init__()
 
         self._doc_class = cls
-        if not urlobject:
-            self._urlobject = cls.urlobject
-        else:
-            self._urlobject = urlobject
         self._cb = cb
+        if not urlobject:
+            urlobject = cls.urlobject
+        try:
+            self._urlobject = urlobject.format(self._cb.credentials.org_key)
+        except (IndexError, KeyError):
+            self._urlobject = urlobject
         self._full_init = False
         self._results = []
         self._query = {}
