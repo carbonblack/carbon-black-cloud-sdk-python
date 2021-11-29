41eb6ae58715941183af753d89d5724a71774f17
Alex Van Brunt <avanbrunt@vmware.com>
2021-11-29 12:58:26 -0700

    Optimize query slicing

--- src/tests/unit/base/test_queries.py
+++ src/tests/unit/base/test_queries.py
@@ -1,6 +1,6 @@
 """Testing the Query objects of cbc_sdk.base"""
 
-from cbc_sdk.base import BaseQuery, SimpleQuery
+from cbc_sdk.base import BaseQuery, SimpleQuery, IterableQueryMixin
 from cbc_sdk.enterprise_edr import Feed, FeedQuery
 from cbc_sdk.rest_api import CBCloudAPI
 from tests.unit.fixtures.stubresponse import patch_cbc_sdk_api
@@ -68,3 +68,107 @@ def test_simple_query(monkeypatch):
     assert clonedSimple._cb == simpleQuery._cb
     assert clonedSimple._results == simpleQuery._results
     assert clonedSimple._query == simpleQuery._query
+
+
+class TestQuery(BaseQuery, IterableQueryMixin):
+    """Test Query for Slicing"""
+
+    def _count():
+        """Mock Count"""
+        pass
+
+def test_query_index(monkeypatch):
+
+    def _mock_query(**kwargs):
+        assert kwargs.get("from_row") == 5
+        assert kwargs.get("max_rows") == 1
+        return [0, 1, 2, 3, 4, 5]
+
+    query = TestQuery()
+    monkeypatch.setattr(query, "_perform_query", _mock_query)
+    query[5] == 5
+
+def test_query_slice_start_and_stop(monkeypatch):
+
+    def _mock_query(**kwargs):
+        assert kwargs.get("from_row") == 5
+        assert kwargs.get("max_rows") == 5
+        return []
+
+    query = TestQuery()
+    monkeypatch.setattr(query, "_perform_query", _mock_query)
+    query[5:10]
+
+def test_query_slice_start(monkeypatch):
+
+    def _mock_query(**kwargs):
+        assert kwargs.get("from_row") == 5
+        assert kwargs.get("max_rows") == -1
+        return []
+
+    query = TestQuery()
+    monkeypatch.setattr(query, "_perform_query", _mock_query)
+    query[5:]
+
+def test_query_slice_stop(monkeypatch):
+
+    def _mock_query(**kwargs):
+        assert kwargs.get("from_row") == 0
+        assert kwargs.get("max_rows") == 1
+        return []
+
+    query = TestQuery()
+    monkeypatch.setattr(query, "_perform_query", _mock_query)
+    query[:1]
+
+def test_query_slice_neg_start(monkeypatch):
+
+    mock_list = [0, 1, 2, 3]
+
+    def _mock_query(**kwargs):
+        assert kwargs.get("from_row") == None
+        assert kwargs.get("max_rows") == None
+        for item in mock_list:
+            yield item
+
+    def _mock_count():
+        return len(mock_list)
+
+    query = TestQuery()
+    monkeypatch.setattr(query, "_perform_query", _mock_query)
+    monkeypatch.setattr(query, "_count", _mock_count)
+    assert query[-2:] == [2, 3]
+
+def test_query_slice_neg_stop(monkeypatch):
+
+    mock_list = [0, 1, 2, 3]
+
+    def _mock_query(**kwargs):
+        assert kwargs.get("from_row") == None
+        assert kwargs.get("max_rows") == None
+        for item in mock_list:
+            yield item
+
+    def _mock_count():
+        return len(mock_list)
+
+    query = TestQuery()
+    monkeypatch.setattr(query, "_perform_query", _mock_query)
+    monkeypatch.setattr(query, "_count", _mock_count)
+    assert query[:-2] == [0, 1]
+
+def test_query_slice_type_error(monkeypatch):
+
+    mock_list = [0, 1, 2, 3]
+
+    def _mock_query():
+        for item in mock_list:
+            yield item
+
+    def _mock_count():
+        return len(mock_list)
+
+    query = TestQuery()
+    monkeypatch.setattr(query, "_perform_query", _mock_query)
+    monkeypatch.setattr(query, "_count", _mock_count)
+    assert query[1:3] == [1, 2]
