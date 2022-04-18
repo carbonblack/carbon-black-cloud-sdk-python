b04e731e554d8d3e94e85dcd98b294992f28fc08
Dimitar Ganev <dganev@vmware.com>
2022-04-18 16:02:10 +0300

    Remove py36 support

--- setup.py
+++ setup.py
@@ -62,7 +62,6 @@ setup(
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Topic :: Security',
