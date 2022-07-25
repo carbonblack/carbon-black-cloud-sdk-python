d220db07d69ed5c68ebff114267adbe256e5de01
Amy Bowersox <abowersox@vmware.com>
2022-07-25 16:20:22 -0600

    fixed unit test that was broken when I fixed the UAT

--- src/tests/unit/enterprise_edr/test_enterprise_edr_threatintel.py
+++ src/tests/unit/enterprise_edr/test_enterprise_edr_threatintel.py
@@ -84,8 +84,9 @@ def test_watchlist_save(cbcsdk_mock):
     api = cbcsdk_mock.api
     id = "watchlistId"
     cbcsdk_mock.mock_request("POST", "/threathunter/watchlistmgr/v3/orgs/test/watchlists", WATCHLIST_GET_SPECIFIC_RESP)
-    watchlist = Watchlist(api, model_unique_id="watchlistId", initial_data=CREATE_WATCHLIST_DATA)
+    watchlist = Watchlist(api, model_unique_id=None, initial_data=CREATE_WATCHLIST_DATA)
     watchlist.validate()
+    print(f"{watchlist._model_unique_id =}")
     watchlist.save()
 
     # if Watchlist response is missing a required field per enterprise_edr.models.Watchlist, raise InvalidObjectError
