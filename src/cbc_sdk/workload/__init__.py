64c46902e4eac63b57536c8048a71b34df7d375a
Amy Bowersox <abowersox@vmware.com>
2022-09-20 15:46:40 -0600

    upgrades to UAT script (not yet tested)

--- src/cbc_sdk/workload/__init__.py
+++ src/cbc_sdk/workload/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 
-from cbc_sdk.workload.vm_workloads_search import ComputeResource
+from cbc_sdk.workload.vm_workloads_search import ComputeResource, AWSComputeResource
 from cbc_sdk.workload.sensor_lifecycle import SensorKit
 from cbc_sdk.workload.nsx_remediation import NSXRemediationJob
 
