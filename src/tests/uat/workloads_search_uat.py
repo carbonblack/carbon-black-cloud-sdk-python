35a326cb199f55d56472e2c94b27a029abf4196a
Amy Bowersox <abowersox@vmware.com>
2022-09-21 13:59:33 -0600

    worked out the kinks in facet results for SDK

--- src/tests/uat/workloads_search_uat.py
+++ src/tests/uat/workloads_search_uat.py
@@ -33,6 +33,7 @@ Workloads Search API:
 # Standard library imports
 import sys
 import requests
+import copy
 
 # Internal library imports
 from cbc_sdk.workload.vm_workloads_search import ComputeResource, AWSComputeResource
@@ -47,7 +48,7 @@ def search_compute_resources_api(cb):
     count = 0
     api_results = []
     while True:
-        response = requests.post(url, json={"rows": "200", "start": count, "criteria": {"deployment_type": "WORKLOAD"}},
+        response = requests.post(url, json={"rows": 200, "start": count, "criteria": {"deployment_type": ["WORKLOAD"]}},
                                  headers=header).json()
         count += len(response['results'])
         api_results.extend(response['results'])
@@ -79,7 +80,7 @@ def search_aws_resources_api(cb):
     count = 0
     api_results = []
     while True:
-        response = requests.post(url, json={"rows": "200", "start": count, "criteria": {"deployment_type": "AWS"}},
+        response = requests.post(url, json={"rows": 200, "start": count, "criteria": {"deployment_type": ["AWS"]}},
                                  headers=header).json()
         count += len(response['results'])
         api_results.extend(response['results'])
@@ -144,9 +145,9 @@ def facet_compute_resource_api(cb):
     header = {'X-Auth-Token': cb.credentials.token,
               'Content-Type': 'application/json'}
     url = f'{cb.credentials.url}lcm/view/v2/orgs/{cb.credentials.org_key}/compute_resources/_facet'
-    response = requests.post(url, json={"criteria": {"deployment_type": "WORKLOAD"},
+    response = requests.post(url, json={"criteria": {"deployment_type": ["WORKLOAD"]},
                                         "terms": {"rows": 200,
-                                                  "fields": ['eligibility', 'os_type', 'os_architecture']}},
+                                                  "fields": ['eligibility', 'os_type', 'installation_status']}},
                              headers=header).json()
     return response['terms']
 
@@ -157,8 +158,11 @@ def facet_compute_resource_sdk(cb):
     print("API Calls:")
     print("Facet Compute Resource")
 
-    facets = cb.select(ComputeResource).facet(['eligibility', 'os_type', 'os_architecture'], 200)
-    return [facet._info for facet in facets]
+    facets = cb.select(ComputeResource).facet(['eligibility', 'os_type', 'installation_status'], 200)
+    return_array = [copy.deepcopy(facet._info) for facet in facets]
+    for item in return_array:
+        item.pop("id", None)
+    return return_array
 
 
 def facet_aws_resource_api(cb):
@@ -166,7 +170,7 @@ def facet_aws_resource_api(cb):
     header = {'X-Auth-Token': cb.credentials.token,
               'Content-Type': 'application/json'}
     url = f'{cb.credentials.url}lcm/view/v2/orgs/{cb.credentials.org_key}/compute_resources/_facet'
-    response = requests.post(url, json={"criteria": {"deployment_type": "AWS"},
+    response = requests.post(url, json={"criteria": {"deployment_type": ["AWS"]},
                                         "terms": {"rows": 200,
                                                   "fields": ['installation_status', 'platform', 'region']}},
                              headers=header).json()
@@ -188,7 +192,7 @@ def summarize_aws_api(cb):
     header = {'X-Auth-Token': cb.credentials.token,
               'Content-Type': 'application/json'}
     url = f'{cb.credentials.url}lcm/view/v2/orgs/{cb.credentials.org_key}/compute_resources/_summarize'
-    response = requests.post(url, json={"criteria": {"deployment_type": "AWS"},
+    response = requests.post(url, json={"criteria": {"deployment_type": ["AWS"]},
                                         "summary_fields": ['installation_status', 'platform', 'region']},
                              headers=header).json()
     return response['summaries']
@@ -234,9 +238,8 @@ def compare_data_summary(sdk_resp, api_resp, api_call):
 def main():
     """Script entry point"""
     parser = build_cli_parser()
-    parser.add_argument('--vcenter-only', action='store_true', type=bool,
-                        help='Run tests only on vCenter compute resources')
-    parser.add_argument('--aws-only', action='store_true', type=bool, help='Run tests only on AWS compute resources')
+    parser.add_argument('--vcenter-only', action='store_true', help='Run tests only on vCenter compute resources')
+    parser.add_argument('--aws-only', action='store_true', help='Run tests only on AWS compute resources')
     args = parser.parse_args()
 
     run_vcenter = True
@@ -253,22 +256,24 @@ def main():
     cb = get_cb_cloud_object(args)
 
     if run_vcenter:
-        sdk_resp = search_compute_resources_sdk(cb)
-        api_resp, resource_id = search_compute_resources_api(cb)
+        sdk_resp, resource_id = search_compute_resources_sdk(cb)
+        api_resp = search_compute_resources_api(cb)
         compare_data_search(sdk_resp, api_resp, 'Search Compute Resources')
 
-        sdk_resp = fetch_compute_resource_by_id_sdk(cb, resource_id)
-        api_resp = fetch_compute_resource_by_id_api(cb, resource_id)
-        compare_data_id(sdk_resp, api_resp, 'Fetch Compute Resource by ID')
+        if resource_id is not None:
+            sdk_resp = fetch_compute_resource_by_id_sdk(cb, resource_id)
+            api_resp = fetch_compute_resource_by_id_api(cb, resource_id)
+            compare_data_id(sdk_resp, api_resp, 'Fetch Compute Resource by ID')
 
     if run_aws:
-        sdk_resp = search_aws_resources_sdk(cb)
-        api_resp, resource_id = search_aws_resources_api(cb)
+        sdk_resp, resource_id = search_aws_resources_sdk(cb)
+        api_resp = search_aws_resources_api(cb)
         compare_data_search(sdk_resp, api_resp, 'Search AWS Compute Resources')
 
-        sdk_resp = fetch_aws_resource_by_id_sdk(cb, resource_id)
-        api_resp = fetch_aws_resource_by_id_api(cb, resource_id)
-        compare_data_id(sdk_resp, api_resp, 'Fetch AWS Compute Resource by ID')
+        if resource_id is not None:
+            sdk_resp = fetch_aws_resource_by_id_sdk(cb, resource_id)
+            api_resp = fetch_aws_resource_by_id_api(cb, resource_id)
+            compare_data_id(sdk_resp, api_resp, 'Fetch AWS Compute Resource by ID')
 
     if run_vcenter:
         sdk_resp = facet_compute_resource_sdk(cb)
