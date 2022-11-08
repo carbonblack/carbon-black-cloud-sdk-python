e4f2817361cd44c2d1d5860d8c28ee657a5d82bb
Amy Bowersox <abowersox@vmware.com>
2022-11-08 16:16:09 -0700

    added base code

--- src/cbc_sdk/platform/__init__.py
+++ src/cbc_sdk/platform/__init__.py
@@ -5,6 +5,8 @@ from cbc_sdk.platform.base import PlatformModel
 from cbc_sdk.platform.alerts import (BaseAlert, WatchlistAlert, CBAnalyticsAlert, DeviceControlAlert,
                                      ContainerRuntimeAlert, Workflow, WorkflowStatus)
 
+from cbc_sdk.platform.asset_groups import AssetGroup
+
 from cbc_sdk.platform.devices import Device, DeviceFacet, DeviceSearchQuery
 
 from cbc_sdk.platform.events import Event, EventFacet
