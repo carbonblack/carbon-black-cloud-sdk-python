2aaab62c90155493c9bface96bb3fe5ad384c78e
Alex Van Brunt <avanbrunt@vmware.com>
2023-02-06 12:55:35 -0700

    Replace single quotes

--- examples/platform/policy_service_crud_operations.py
+++ examples/platform/policy_service_crud_operations.py
@@ -72,7 +72,7 @@ def list_policies(cb, parser, args):
         print(u"Policy id {0}: {1} {2}".format(p.id, p.name, "({0})".format(p.description) if p.description else ""))
         print("Rules:")
         for r in p.rules:
-            print("  {0}: {1} when {2} {3} is {4}".format(r.get('id'), r.get("action"),
+            print("  {0}: {1} when {2} {3} is {4}".format(r.get("id"), r.get("action"),
                                                           r.get("application", {}).get("type"),
                                                           r.get("application", {}).get("value"), r.get("operation")))
 
