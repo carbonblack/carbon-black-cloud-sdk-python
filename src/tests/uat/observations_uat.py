c763e83e568260207efb975cadcb4b71e964bc24
Emanuela Mitreva <emitreva@vmware.com>
2023-03-15 07:35:01 +0200

    Update uat to match new changes

--- src/tests/uat/observations_uat.py
+++ src/tests/uat/observations_uat.py
@@ -21,7 +21,7 @@ import sys
 import time
 import requests
 from cbc_sdk.helpers import build_cli_parser, get_cb_cloud_object
-from cbc_sdk.platform import Observation, NetworkThreatMetadata
+from cbc_sdk.platform import Observation, NetworkThreatMetadata, ObservationFacet
 
 # ------------------------------ APIs ----------------------------------------------
 
@@ -40,7 +40,6 @@ GET_FACET_RESULTS = "{}api/investigate/v2/orgs/{}/observations/facet_jobs/{}/res
 
 # others
 SEARCH_SUGGESTIONS = "{}api/investigate/v2/orgs/{}/observations/search_suggestions?suggest.q={}"
-SEARCH_VALIDATION = "{}api/investigate/v2/orgs/{}/observations/search_validation?{}"
 GET_NETWORK_THREAT_METADATA = "{}threatmetadata/v1/orgs/{}/detectors/{}"
 
 # ------------------------------ Formatters ------------------------------------------
@@ -54,10 +53,10 @@ OBSERVATION_ID = 0
 SECTION_TITLES = ["Observations", "Network Threat Metadata"]
 TITLES = [
     "Get Search Suggestions",
-    "Get Search Validations",
     "Get Search Results",
     "Get Search Grouped Results",
     "Get Details Results",
+    "Get Facet Data",
     "Get Network Threat Metadata",
 ]
 
@@ -69,13 +68,9 @@ def get_search_results():
     global OBSERVATION_ID
     sdata = {"query": "rule_id:* AND observation_type:TAU_INTELLIGENCE"}
     gdata = {"fields": ["device_name"], "range": {}, "rows": 50}
-    job_id = requests.post(
-        START_SEARCH_JOB.format(HOSTNAME, ORG_KEY), headers=HEADERS, json=sdata
-    ).json()["job_id"]
+    job_id = requests.post(START_SEARCH_JOB.format(HOSTNAME, ORG_KEY), headers=HEADERS, json=sdata).json()["job_id"]
     time.sleep(0.5)
-    results = requests.get(
-        GET_SEARCH_RESULTS.format(HOSTNAME, ORG_KEY, job_id), headers=HEADERS
-    ).json()
+    results = requests.get(GET_SEARCH_RESULTS.format(HOSTNAME, ORG_KEY, job_id), headers=HEADERS).json()
     OBSERVATION_ID = results["results"][0]["observation_id"]
     gresults = requests.post(
         GET_GROUPED_RESULTS.format(HOSTNAME, ORG_KEY, job_id),
@@ -88,12 +83,22 @@ def get_search_results():
 def get_details_results(observation_id):
     """Get details results"""
     ddata = {"observation_ids": [observation_id]}
-    job_id = requests.post(
-        START_DETAILS_JOB.format(HOSTNAME, ORG_KEY), headers=HEADERS, json=ddata
-    ).json()["job_id"]
+    job_id = requests.post(START_DETAILS_JOB.format(HOSTNAME, ORG_KEY), headers=HEADERS, json=ddata).json()["job_id"]
     time.sleep(0.5)
     results = requests.get(GET_DETAILS_RESULTS.format(HOSTNAME, ORG_KEY, job_id), headers=HEADERS)
     return results.json()["results"][0]
+
+
+def get_facet_results():
+    """Get facet results"""
+    fdata = {
+        "query": "rule_id:* AND observation_type:TAU_INTELLIGENCE",
+        "terms": {"fields": ["device_name"]},
+    }
+    job_id = requests.post(START_FACET_JOB.format(HOSTNAME, ORG_KEY), headers=HEADERS, json=fdata).json()["job_id"]
+    time.sleep(0.5)
+    results = requests.get(GET_FACET_RESULTS.format(HOSTNAME, ORG_KEY, job_id), headers=HEADERS)
+    return results.json()
 
 
 def get_seach_suggestions(q="device_id&suggest.count=1"):
@@ -102,18 +107,9 @@ def get_seach_suggestions(q="device_id&suggest.count=1"):
     return results.json()["suggestions"]
 
 
-def get_seach_validation(q="*:*", mint="1641469642000", maxt="1678103242000"):
-    """Get search suggestions"""
-    query = f"q={q}&cb.min_backend_timestamp={mint}&cb.max_backend_timestamp={maxt}"
-    results = requests.get(SEARCH_VALIDATION.format(HOSTNAME, ORG_KEY, query), headers=HEADERS)
-    return results.json()["valid"]
-
-
 def get_network_threat_metadata(rule_id):
     """Get network threat metadata"""
-    results = requests.get(
-        GET_NETWORK_THREAT_METADATA.format(HOSTNAME, ORG_KEY, rule_id), headers=HEADERS
-    ).json()
+    results = requests.get(GET_NETWORK_THREAT_METADATA.format(HOSTNAME, ORG_KEY, rule_id), headers=HEADERS).json()
     results["tms_rule_id"] = rule_id
     return results
 
@@ -142,42 +138,43 @@ def main():
     print(SYMBOLS * DELIMITER)
 
     api_result = get_seach_suggestions()
-    sdk_result = cb.observations_search_suggestions(query="device_id", count=1)
-    assert (
-        api_result == sdk_result
-    ), f"Test Failed Expected: {api_result} Actual: {sdk_result}"
-    print(TITLES[0] + "." * (SYMBOLS - len(TITLES[0]) - 2) + "OK")
-    api_result = get_seach_validation()
-    sdk_result = cb.observations_search_validation(
-        query="*:*",
-        min_backend_timestamp="1641469642000",
-        max_backend_timestamp="1678103242000",
-    )
+    sdk_result = Observation.search_suggestions(cb, query="device_id", count=1)
     assert api_result == sdk_result, f"Test Failed Expected: {api_result} Actual: {sdk_result}"
-    print(TITLES[1] + "." * (SYMBOLS - len(TITLES[1]) - 2) + "OK")
+    print(TITLES[0] + "." * (SYMBOLS - len(TITLES[0]) - 2) + "OK")
 
     # check get search job
     sapi_result, gapi_result = get_search_results()
     sdk_r = cb.select(Observation).where("rule_id:* AND observation_type:TAU_INTELLIGENCE")
     ssdk_result = [x._info for x in sdk_r]
     assert sapi_result == ssdk_result, f"Test Failed Expected: {sapi_result} Actual: {ssdk_result}"
-    print(TITLES[2] + "." * (SYMBOLS - len(TITLES[2]) - 2) + "OK")
+    print(TITLES[1] + "." * (SYMBOLS - len(TITLES[1]) - 2) + "OK")
+
     # check get group results
     sdk_result = [y._info for x in sdk_r.get_group_results("device_name") for y in x.observations]
     api_result = []
     for group in gapi_result:
         api_result.extend(group["results"])
-    assert (api_result == sdk_result), f"Test Failed Expected: {api_result} Actual: {sdk_result}"
-    print(TITLES[3] + "." * (SYMBOLS - len(TITLES[3]) - 2) + "OK")
+    assert api_result == sdk_result, f"Test Failed Expected: {api_result} Actual: {sdk_result}"
+    print(TITLES[2] + "." * (SYMBOLS - len(TITLES[2]) - 2) + "OK")
 
     # check get details job
     obs_id = sapi_result[0]["observation_id"]
     rule_id = sapi_result[0]["rule_id"]
     api_result = get_details_results(obs_id)
     sdk_result = cb.select(Observation, obs_id).get_details()._info
-    assert (
-        api_result == sdk_result
-    ), f"Test Failed Expected: {api_result} Actual: {sdk_result}"
+    assert api_result == sdk_result, f"Test Failed Expected: {api_result} Actual: {sdk_result}"
+    print(TITLES[3] + "." * (SYMBOLS - len(TITLES[3]) - 2) + "OK")
+
+    # check get facet job
+    api_result = get_facet_results()["terms"]
+    xx = (
+        cb.select(ObservationFacet)
+        .where("rule_id:* AND observation_type:TAU_INTELLIGENCE")
+        .add_facet_field("device_name")
+        .results
+    )
+    sdk_result = xx.terms
+    assert api_result == sdk_result, f"Test Failed Expected: {api_result} Actual: {sdk_result}"
     print(TITLES[4] + "." * (SYMBOLS - len(TITLES[4]) - 2) + "OK")
 
     print()
