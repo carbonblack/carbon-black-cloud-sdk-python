02a218d3b86136ce24167d30894edf84b1a20fc1
Amy Bowersox <abowersox@vmware.com>
2023-06-23 10:42:26 -0600

    all updates except for changelog

--- src/cbc_sdk/__init__.py
+++ src/cbc_sdk/__init__.py
@@ -4,7 +4,7 @@ __title__ = 'cbc_sdk'
 __author__ = 'Carbon Black Developer Network'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2020-2023 VMware Carbon Black'
-__version__ = '1.4.2'
+__version__ = '1.4.3'
 
 from .rest_api import CBCloudAPI
 from .cache import lru
