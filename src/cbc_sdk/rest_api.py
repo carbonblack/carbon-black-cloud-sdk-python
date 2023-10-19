7e2b7e666420a57d2cff815e6a0e82e4822e60ea
Alex Van Brunt <avanbrunt@carbonblack.com>
2023-10-19 17:16:46 -0600

    Add threat tests

--- src/cbc_sdk/rest_api.py
+++ src/cbc_sdk/rest_api.py
@@ -419,7 +419,7 @@ class CBCloudAPI(BaseAPI):
             request["remediation_state"] = remediation
         if comment is not None:
             request["comment"] = comment
-        url = "/api/alerts/v7/orgs/{0}/alerts/workflow".format(self.credentials.org_key)
+        url = "/appservices/v6/orgs/{0}/threat/workflow/_criteria".format(self.credentials.org_key)
         resp = self.post_object(url, body=request)
         output = resp.json()
         return output["request_id"]
