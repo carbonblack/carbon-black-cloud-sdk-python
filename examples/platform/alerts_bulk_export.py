15df24daae16b8c0bbd021e7a046a46870867a83
Kylie Ebringer <kebringer@vmware.com>
2023-10-23 20:32:51 -0600

    removed test profile

--- examples/platform/alerts_bulk_export.py
+++ examples/platform/alerts_bulk_export.py
@@ -29,7 +29,7 @@ from datetime import datetime, timedelta, timezone
 
 def main():
     """This script demonstrates how to use Alerts in the SDK and common operations to link to related objects."""
-    api = CBCloudAPI(profile="EXPLORE_DEMO")
+    api = CBCloudAPI(profile="YOUR_PROFILE_HERE")
 
     # Time field and format to use
     time_field = "backend_timestamp"
