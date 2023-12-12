dd5c87fcc7aa3e0355aff9da242495e84e8dad7d
Amy Bowersox <abowersox@vmware.com>
2023-12-12 16:49:19 -0700

    put in tests for list_member_ids, list_members, and add_members

--- src/tests/unit/platform/test_asset_groups.py
+++ src/tests/unit/platform/test_asset_groups.py
@@ -16,11 +16,13 @@ import logging
 import copy
 from cbc_sdk.rest_api import CBCloudAPI
 from cbc_sdk.errors import ApiError
-from cbc_sdk.platform import AssetGroup
+from cbc_sdk.platform import AssetGroup, Device
 from tests.unit.fixtures.CBCSDKMock import CBCSDKMock
 from tests.unit.fixtures.platform.mock_asset_groups import (CREATE_AG_REQUEST, CREATE_AG_RESPONSE, EXISTING_AG_DATA,
                                                             UPDATE_AG_REQUEST, QUERY_REQUEST, QUERY_REQUEST_DEFAULT,
-                                                            QUERY_RESPONSE)
+                                                            QUERY_RESPONSE, LIST_MEMBERS_RESPONSE1,
+                                                            LIST_MEMBERS_RESPONSE2)
+from tests.unit.fixtures.platform.mock_devices import GET_DEVICE_RESP
 
 
 logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s', level=logging.DEBUG, filename='log.txt')
@@ -198,3 +200,99 @@ def test_query_fail_criteria_set(cb):
     query = cb.select(AssetGroup)
     with pytest.raises(ApiError):
         query.sort_by("name", "NOTADIRECTION")
+
+
+def test_list_member_ids_basic(cbcsdk_mock):
+    """Tests the formatting of the 'list members' call with rows and start parameters, and the basic response."""
+    cbcsdk_mock.mock_request('GET', '/asset_groups/v1/orgs/test/groups/db416fa2-d5f2-4fb5-8a5e-cd89f6ecda16',
+                             copy.deepcopy(EXISTING_AG_DATA))
+    cbcsdk_mock.mock_request('GET', '/asset_groups/v1/orgs/test/groups/db416fa2-d5f2-4fb5-8a5e-cd89f6ecda16/members?rows=20&start=0',  # noqa: E501
+                             LIST_MEMBERS_RESPONSE1)
+    api = cbcsdk_mock.api
+    group = api.select(AssetGroup, 'db416fa2-d5f2-4fb5-8a5e-cd89f6ecda16')
+    rc = group.list_member_ids(rows=20, start=0)
+    assert rc == [12345678, 66760099, 42691014]
+
+
+@pytest.mark.parametrize("filter_param, expected", [
+    ("ALL", [12345678, 66760099, 42691014]),
+    ("DYNAMIC", [12345678, 42691014]),
+    ("MANUAL", [66760099])
+])
+def test_list_member_ids_filtering(cbcsdk_mock, filter_param, expected):
+    """Tests the filter action on the list_member_ids function."""
+    cbcsdk_mock.mock_request('GET', '/asset_groups/v1/orgs/test/groups/db416fa2-d5f2-4fb5-8a5e-cd89f6ecda16',
+                             copy.deepcopy(EXISTING_AG_DATA))
+    cbcsdk_mock.mock_request('GET', '/asset_groups/v1/orgs/test/groups/db416fa2-d5f2-4fb5-8a5e-cd89f6ecda16/members',
+                             LIST_MEMBERS_RESPONSE1)
+    api = cbcsdk_mock.api
+    group = api.select(AssetGroup, 'db416fa2-d5f2-4fb5-8a5e-cd89f6ecda16')
+    rc = group.list_member_ids(filter=filter_param)
+    assert rc == expected
+
+
+def test_list_member_ids_invalid_filter(cbcsdk_mock):
+    """Tests the "invalid filter" exception in list_member_ids."""
+    cbcsdk_mock.mock_request('GET', '/asset_groups/v1/orgs/test/groups/db416fa2-d5f2-4fb5-8a5e-cd89f6ecda16',
+                             copy.deepcopy(EXISTING_AG_DATA))
+    api = cbcsdk_mock.api
+    group = api.select(AssetGroup, 'db416fa2-d5f2-4fb5-8a5e-cd89f6ecda16')
+    with pytest.raises(ApiError):
+        group.list_member_ids(filter="BOGUS")
+
+
+def test_list_members(cbcsdk_mock):
+    """Tests the device return mechanism of list_members."""
+    cbcsdk_mock.mock_request('GET', '/asset_groups/v1/orgs/test/groups/db416fa2-d5f2-4fb5-8a5e-cd89f6ecda16',
+                             copy.deepcopy(EXISTING_AG_DATA))
+    cbcsdk_mock.mock_request('GET', '/asset_groups/v1/orgs/test/groups/db416fa2-d5f2-4fb5-8a5e-cd89f6ecda16/members',
+                             LIST_MEMBERS_RESPONSE2)
+    cbcsdk_mock.mock_request("GET", "/appservices/v6/orgs/test/devices/98765", GET_DEVICE_RESP)
+    api = cbcsdk_mock.api
+    group = api.select(AssetGroup, 'db416fa2-d5f2-4fb5-8a5e-cd89f6ecda16')
+    rc = group.list_members()
+    assert len(rc) == 1
+    assert isinstance(rc[0], Device)
+    assert rc[0].id == 98765
+
+
+@pytest.mark.parametrize("param1, param2, expected", [
+    ("12345678", 14760, ["12345678", "14760"]),
+    (303873, "777000", ["303873", "777000"]),
+    (None, [16, "24", 99], ["16", "24", "99"]),
+    ("obviously_bogus", (12, 23, 34), ["12", "23", "34"]),
+    ({4400}, {16384}, ["4400", "16384"]),
+    (65536, 3.1416, ["65536", "3"])
+])
+def test_add_members(cbcsdk_mock, param1, param2, expected):
+    """Tests the add_members API with various combinations of parameters."""
+    def on_post(url, body, **kwargs):
+        assert body['action'] == 'CREATE'
+        assert body['external_member_ids'] == expected
+        return CBCSDKMock.StubResponse("", scode=204, json_parsable=False)
+
+    cbcsdk_mock.mock_request('GET', '/asset_groups/v1/orgs/test/groups/db416fa2-d5f2-4fb5-8a5e-cd89f6ecda16',
+                             copy.deepcopy(EXISTING_AG_DATA))
+    cbcsdk_mock.mock_request('POST', '/asset_groups/v1/orgs/test/groups/db416fa2-d5f2-4fb5-8a5e-cd89f6ecda16/members',
+                             on_post)
+    api = cbcsdk_mock.api
+    group = api.select(AssetGroup, 'db416fa2-d5f2-4fb5-8a5e-cd89f6ecda16')
+    group.add_members(param1, param2)
+
+
+def test_add_members_with_device(cbcsdk_mock):
+    """Tests the add_members API with a Device object."""
+    def on_post(url, body, **kwargs):
+        assert body['action'] == 'CREATE'
+        assert body['external_member_ids'] == ["98765"]
+        return CBCSDKMock.StubResponse("", scode=204, json_parsable=False)
+
+    cbcsdk_mock.mock_request('GET', '/asset_groups/v1/orgs/test/groups/db416fa2-d5f2-4fb5-8a5e-cd89f6ecda16',
+                             copy.deepcopy(EXISTING_AG_DATA))
+    cbcsdk_mock.mock_request("GET", "/appservices/v6/orgs/test/devices/98765", GET_DEVICE_RESP)
+    cbcsdk_mock.mock_request('POST', '/asset_groups/v1/orgs/test/groups/db416fa2-d5f2-4fb5-8a5e-cd89f6ecda16/members',
+                             on_post)
+    api = cbcsdk_mock.api
+    group = api.select(AssetGroup, 'db416fa2-d5f2-4fb5-8a5e-cd89f6ecda16')
+    device = api.select(Device, 98765)
+    group.add_members(device)
