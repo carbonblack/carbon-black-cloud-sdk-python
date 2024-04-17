d653beb2566062e53a5a1e6e6c0052fb58eee2ba
Amy Bowersox <abowersox@vmware.com>
2024-04-17 14:33:34 -0600

    first unit test added and fixed up

--- src/tests/unit/platform/test_audit.py
+++ src/tests/unit/platform/test_audit.py
@@ -12,10 +12,11 @@
 """Tests for the audit logs APIs."""
 
 import pytest
+import copy
 from cbc_sdk.rest_api import CBCloudAPI
-from cbc_sdk.platform.audit import AuditLog
+from cbc_sdk.platform import AuditLog, AuditLogRecord
 from tests.unit.fixtures.CBCSDKMock import CBCSDKMock
-from tests.unit.fixtures.platform.mock_audit import AUDITLOGS_RESP
+from tests.unit.fixtures.platform.mock_audit import AUDITLOGS_RESP, AUDIT_SEARCH_REQUEST, AUDIT_SEARCH_RESPONSE
 
 
 @pytest.fixture(scope="function")
@@ -35,15 +36,42 @@ def cbcsdk_mock(monkeypatch, cb):
 # ==================================== UNIT TESTS BELOW ====================================
 
 
-def test_no_create_object_for_now(cb):
-    """Validates that we can't create an AuditLog object. Remove when we have a better implementation."""
-    with pytest.raises(NotImplementedError):
-        AuditLog(cb, 0)
-
-
 def test_get_auditlogs(cbcsdk_mock):
     """Tests getting audit logs."""
     cbcsdk_mock.mock_request("GET", "/integrationServices/v3/auditlogs", AUDITLOGS_RESP)
     api = cbcsdk_mock.api
     result = AuditLog.get_auditlogs(api)
     assert len(result) == 5
+
+
+def test_search_audit_logs_with_all_bells_and_whistles(cbcsdk_mock):
+    """Tests the generation and execution of a search request."""
+
+    def on_post(url, body, **kwargs):
+        assert body == AUDIT_SEARCH_REQUEST
+        return AUDIT_SEARCH_RESPONSE
+
+    cbcsdk_mock.mock_request("POST", "/audit_log/v1/orgs/test/logs/_search", on_post)
+    api = cbcsdk_mock.api
+    query = api.select(AuditLogRecord).where("description:FOO").add_criteria("actor_ip", ["10.29.99.1"])
+    query.add_criteria("actor", ["ABCDEFGHIJ"]).add_criteria("request_url", ["https://inclusiveladyship.com"])
+    query.add_criteria("description", ["FOOBAR"]).add_boolean_criteria("flagged", True)
+    query.add_boolean_criteria("verbose", False)
+    query.add_time_criteria(start="2024-03-01T00:00:00", end="2024-03-31T22:00:00")
+    query.add_exclusions("actor_ip", ["10.29.99.254"]).add_exclusions("actor", ["JIHGFEDCBA"])
+    query.add_exclusions("request_url", ["https://links.inclusiveladyship.com"])
+    query.add_exclusions("description", ["BLORT"]).add_boolean_criteria("flagged", False, exclude=True)
+    query.add_boolean_criteria("verbose", True, exclude=True)
+    query.add_time_criteria(range="-5d", exclude=True).sort_by("actor_ip", "ASC")
+    l = list(query)
+    assert len(l) == 5
+    assert l[0].actor == "DEFGHIJKLM"
+    assert l[0].actor_ip == "192.168.0.5"
+    assert l[1].actor == "BELTALOWDA"
+    assert l[1].actor_ip == "192.168.3.5"
+    assert l[2].actor == "BELTALOWDA"
+    assert l[2].actor_ip == "192.168.3.8"
+    assert l[3].actor == "BELTALOWDA"
+    assert l[3].actor_ip == "192.168.3.11"
+    assert l[4].actor == "BELTALOWDA"
+    assert l[4].actor_ip == "192.168.3.14"
