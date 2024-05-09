ae30dd7c8588eb2755a9732030bcdd754370f5fe
Amy Bowersox <abowersox@vmware.com>
2024-05-09 15:29:32 -0600

    update copyright block in example scripts

--- examples/platform/audit_log.py
+++ examples/platform/audit_log.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # *******************************************************
-# Copyright (c) VMware, Inc. 2020-2023. All Rights Reserved.
+# Copyright (c) Broadcom, Inc. 2020-2024. All Rights Reserved. Carbon Black.
 # SPDX-License-Identifier: MIT
 # *******************************************************
 # *
