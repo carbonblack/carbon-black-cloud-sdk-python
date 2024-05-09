ede253a9cb729c217503a002b5b5aac1beb94240
Amy Bowersox <abowersox@vmware.com>
2024-05-09 12:42:46 -0600

    updated copyright notices across the SDK source code files

--- src/cbc_sdk/workload/__init__.py
+++ src/cbc_sdk/workload/__init__.py
@@ -1,3 +1,14 @@
+# *******************************************************
+# Copyright (c) Broadcom, Inc. 2020-2024. All Rights Reserved. Carbon Black.
+# SPDX-License-Identifier: MIT
+# *******************************************************
+# *
+# * DISCLAIMER. THIS PROGRAM IS PROVIDED TO YOU "AS IS" WITHOUT
+# * WARRANTIES OR CONDITIONS OF ANY KIND, WHETHER ORAL OR WRITTEN,
+# * EXPRESS OR IMPLIED. THE AUTHOR SPECIFICALLY DISCLAIMS ANY IMPLIED
+# * WARRANTIES OR CONDITIONS OF MERCHANTABILITY, SATISFACTORY QUALITY,
+# * NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE.
+
 from __future__ import absolute_import
 
 from cbc_sdk.workload.vm_workloads_search import VCenterComputeResource, AWSComputeResource
