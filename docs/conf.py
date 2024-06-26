39451cb8448b0c4980fd3ac7c930eeffbe7d2ca8
Amy Bowersox <abowersox@vmware.com>
2024-06-26 10:06:38 -0600

    bumped version number and included changelog info

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2020-2024 Broadcom Inc. - Carbon Black'
 author = 'Developer Relations'
 
 # The full version, including alpha/beta/rc tags
-release = '1.5.2'
+release = '1.5.3'
 
 
 # -- General configuration ---------------------------------------------------
